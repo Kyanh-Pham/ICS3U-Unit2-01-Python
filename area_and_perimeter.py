2fd652e03cf3196f0745142ce911cf72f9924ec0
Kyanh Pham <kyanh.pham@mths.ca>
2022-09-22 14:56:25 +0000

    calculates the area and perimeter of circle

---
+++ area_and_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+ 
+# Created by: Kyanh Pham
+# Created on: Sept 2022
+# This program calculates the area and perimeter of a circle
+# with a radius of 15mm
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15 mm: ")
+    print("")
+    print("Area is {} mm².".format(math.pi * 15 ** 2))
+    print("Perimeter is {} mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
+    
